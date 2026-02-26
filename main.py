e902f8dec5239de97cc4f995486ac516c4d6988e
misa-y <mishayamamoto0@gmail.com>
2026-02-26 15:04:43 +0900

    Added windowing library and created a window (PyQt6)

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import cv2
 import time
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+import sys
+
+def main():
+     app = QApplication(sys.argv)
+
+     window = QWidget()
+     window.setWindowTitle("ASIJ Photobooth")
+     
+     layout = QVBoxLayout()
+     label = QLabel("Welcome")
+     
+     layout.addWidget(label)
+     window.setLayout(layout)
+     window.resize(300,150)
+     
+     window.show()
+     sys.exit(app.exec())
+
+
+
+     #startCamera()
 
 def startCamera():
     video = cv2.VideoCapture(0)
@@ -15,15 +37,16 @@ def startCamera():
 
         cv2.imshow("ASIJ Photobooth", frame)
 
-        cv2.createButton("Start", captureImages(video))
+        cv2.createButton("Start", onChange=onChange)
                          
         key = cv2.waitKey(1) & 0xFF
-        if key == ord('p'):
+        if key == ord('l'):
             #countdown(video)
             captureImages(video)
             photo_count+=1
 
             print(photo_count)
+        
         elif key == ord('q'):
             break
    
@@ -34,7 +57,7 @@ def captureImages(video):
     for i in range (4):
                 start_time= time.time()
               
-                while time.time() - start_time < 7: 
+                while time.time() - start_time < 2: 
                     ret, frame = video.read()
                     frame = cv2.flip(frame, 1)
                     cv2.imshow("ASIJ Photobooth", frame)
@@ -49,11 +72,13 @@ def captureImages(video):
 
                 print (f"Saved {filename}")
 
+def onChange():
+     print ("hi")
 
 
-startCamera()
-
-
+#run
+if __name__ == "__main__":
+    main()
 
 
 
