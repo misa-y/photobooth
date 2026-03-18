f6c068973cfa0ec4ed53d1e5bceae0c5a658bb66
misa-y <mishayamamoto0@gmail.com>
2026-03-18 09:01:19 +0900

    Added 6 color frame options (shows previews of each one as you select), pressing "Next" prints the one you chose

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from curses import window
+
 import cv2
 import numpy as np
 import time
@@ -62,33 +64,86 @@ class Window(QWidget):
         window.photo_count = 0 #total number of photostrips
         window.photos = [] #array to save filenames for photostrip generation
         window.captureIndex = 0 #number of photos taken in current photostrip
-        window.countdown = 7 #7 seconds between each picture
+        window.countdown = 3 #7 seconds between each picture
+        window.frameColor = (255,255,255) #default photostrip frame color is white
         
         window.video = None
         window.setWindowTitle("Photobooth")
         
+        #GUI Layout
         window.layout = QVBoxLayout()
         window.setLayout(window.layout)
         window.setFixedSize(1470,895)   
         
+        #welcome text
         window.welcome = QLabel("Welcome to my Photobooth") #welcome message displayed at the start of the program
         window.welcome.setStyleSheet("font-size: 36px; font-weight: bold;")
         window.layout.addWidget(window.welcome, alignment = Qt.AlignmentFlag.AlignCenter)
 
-        window.button = QPushButton("Start", window)
-        window.button.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #fdbe15; color: white; border: none; border-radius: 5px;""")
-        window.button.clicked.connect(window.clicked)
-        window.layout.addWidget(window.button, alignment = Qt.AlignmentFlag.AlignCenter)
+        #start button
+        window.startButton = QPushButton("Start", window)
+        window.startButton.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #fdbe15; color: white; border: none; border-radius: 4px;""")
+        window.startButton.clicked.connect(window.clicked)
+        window.layout.addWidget(window.startButton, alignment = Qt.AlignmentFlag.AlignCenter)
 
+        #next button
+        window.nextButton = QPushButton("Next", window)
+        window.nextButton.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #fdbe15; color: white; border: none; border-radius: 4px;""")
+        window.nextButton.clicked.connect(window.printPhotostrip)
+        window.nextButton.setHidden(True)
+        window.layout.addWidget(window.nextButton, alignment = Qt.AlignmentFlag.AlignRight)
+
+        #countdown label
         window.countdownLabel = QLabel("") #widget to display countdown before each photo is taken 
         window.layout.addWidget(window.countdownLabel, alignment = Qt.AlignmentFlag.AlignCenter)
 
+        #timer
         window.timer = QTimer()
         window.timer.timeout.connect(window.cameraLoop) #runs cameraLoop when timer is connected
         
+        #image label
         window.imageLabel = QLabel() #widget to display camera feed and previews
         window.imageLabel.setFixedSize(867, 714)
         window.layout.addWidget(window.imageLabel, alignment = Qt.AlignmentFlag.AlignCenter)
+
+        #standard color frames buttons
+        window.white = QPushButton("        ", window) #white
+        window.white.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #ffffff; color: black; border: none; border-radius: 2px;""")
+        window.layout.addWidget(window.white, alignment = Qt.AlignmentFlag.AlignRight)
+        window.white.setHidden(True)
+
+        window.black = QPushButton("        ", window) #black
+        window.black.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #000000; color: white; border: none; border-radius: 2px;""")
+        window.layout.addWidget(window.black, alignment = Qt.AlignmentFlag.AlignRight)
+        window.black.setHidden(True)
+
+        window.pink = QPushButton("        ", window) #pink
+        window.pink.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #ffdbe9; color: black; border: none; border-radius: 2px;""")
+        window.layout.addWidget(window.pink, alignment = Qt.AlignmentFlag.AlignRight) 
+        window.pink.setHidden(True)
+
+        window.yellow = QPushButton("        ", window) #yellow
+        window.yellow.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #fff6b7; color: black; border: none; border-radius: 2px;""")
+        window.layout.addWidget(window.yellow, alignment = Qt.AlignmentFlag.AlignRight) 
+        window.yellow.setHidden(True)
+
+        window.mint = QPushButton("        ", window) #mint
+        window.mint.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #b7e6df; color: black; border: none; border-radius: 2px;""")
+        window.layout.addWidget(window.mint, alignment = Qt.AlignmentFlag.AlignRight) 
+        window.mint.setHidden(True)
+        
+        window.lavender = QPushButton("        ", window) #lavender
+        window.lavender.setStyleSheet("""font-size: 24px; padding: 10px; background-color: #e6e6fa; color: black; border: none; border-radius: 2px;""")
+        window.layout.addWidget(window.lavender, alignment = Qt.AlignmentFlag.AlignRight) 
+        window.lavender.setHidden(True)
+
+        #when color button is pressed, show photostrip with that color frame
+        window.white.clicked.connect(lambda: window.showFrame((255,255,255)))
+        window.black.clicked.connect(lambda: window.showFrame((0,0,0)))
+        window.pink.clicked.connect(lambda: window.showFrame((233,219,255)))
+        window.yellow.clicked.connect(lambda: window.showFrame((183,246,255)))
+        window.mint.clicked.connect(lambda: window.showFrame((223,230,183)))
+        window.lavender.clicked.connect(lambda: window.showFrame((250,230,230)))
 
     def clicked(window):
         """
@@ -97,7 +152,7 @@ class Window(QWidget):
          Function: Hides the welcome message and start button, then initializes the camera system.
         """
         window.welcome.setHidden(True)
-        window.button.setHidden(True)
+        window.startButton.setHidden(True)
         
         window.startCamera()
 
@@ -119,7 +174,7 @@ class Window(QWidget):
         window.video.set(cv2.CAP_PROP_FRAME_WIDTH, 867) #510*1.7
         window.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 714) #420*1.7
      
-        window.timer.start (40) #update every 40ms
+        window.timer.start (30) #update every 30ms
 
         if not window.video.isOpened(): #check if camera opened successfully
             print("Error Opening the Camera")
@@ -219,8 +274,7 @@ class Window(QWidget):
             window.timer.stop()
             window.preview()
             print ("Done capturing photos")
-            QTimer.singleShot(2500,window.photostrip)
-            QTimer.singleShot(6500,window.reset)
+            QTimer.singleShot(2500,window.photostripHelper)
             
             window.photo_count+=1
             print(f"Current # of Photo Strips: {window.photo_count}")
@@ -242,15 +296,32 @@ class Window(QWidget):
 
          Function: Clears the countdown label and restarts the camera timer to continue showing the live feed in preparation for the next photo capture in the photostrip sequence. This function is called after the preview of each captured photo, except after the fourth photo when the photostrip is generated instead.
         """
-        window.countdown = 7
+        window.countdown = 3
         window.timer.start(30)
         window.startCountdown()
+
+    def chooseFrame(window):
+        window.nextButton.setHidden(False)
+        window.white.setHidden(False)
+        window.black.setHidden(False)
+        window.pink.setHidden(False)
+        window.yellow.setHidden(False)
+        window.mint.setHidden(False)
+        window.lavender.setHidden(False)
+
+    def showFrame(window, color):
+        window.frameColor = color
+        window.photostrip()
+    
+    def photostripHelper(window):
+        window.photostrip()
+        window.chooseFrame()
 
     def photostrip(window):
         """
          Generates a photostrip from the captured photos.
 
-         Function: Loads the captured image files and arranges them in a grid to create a photostrip, which is then saved as an timed stamped image file.
+         Function: Loads the captured image files and arranges them in a grid to create a photostrip.
 
          Preconditions:
          - Four photos must have been captured and saved in the photos list.
@@ -268,7 +339,7 @@ class Window(QWidget):
         spacing = 60
         stripheight = (height * 4) + (spacing * 5)
 
-        window.photostripImage = np.zeros((stripheight, width+220, 3), dtype=np.uint8)
+        window.photostripImage = np.full((stripheight, width+220, 3), window.frameColor, dtype=np.uint8)
 
         y = 60
         x = 110
@@ -277,22 +348,22 @@ class Window(QWidget):
             window.photostripImage[y:y+height, x: x+width] = photo
             y += height + spacing
         
-        timestamp = time.strftime("%Y%m%d%H%M%S")
-        filename = f"photostrip_{timestamp}.png"
-        cv2.imwrite(filename, window.photostripImage)
-
-        window.previewPhotostrip()
-
-    def previewPhotostrip(window): 
-        """
-         Displays the generated photostrip in the GUI.
-
-         Function: Converts the generated photostrip image into a format suitable for display in the PyQt interface and updates the imageLabel widget to show the photostrip preview to the user. This function is called after the photostrip is generated and saved to disk, allowing the user to see the final result of their photobooth session before everything is reset for the next session.
-         """
+        #preview photostrip
         height, width, channels = window.photostripImage.shape
         previewPhotostrip = QImage(window.photostripImage.data, width, height, QImage.Format.Format_BGR888)
         pixmapPhotostrip = QPixmap.fromImage(previewPhotostrip)
         window.imageLabel.setPixmap(pixmapPhotostrip.scaled(window.imageLabel.width(), window.imageLabel.height(), Qt.AspectRatioMode.KeepAspectRatio))
+       
+    def printPhotostrip(window): 
+        """
+         Displays the generated photostrip in the GUI and saves it.
+
+         Function: Converts the generated photostrip image into a format suitable for display in the PyQt interface and updates the imageLabel widget to show the photostrip preview to the user. This function is called after the photostrip is generated and saved to disk, allowing the user to see the final result of their photobooth session before everything is reset for the next session.
+         """
+        timestamp = time.strftime("%Y%m%d%H%M%S")
+        filename = f"photostrip_{timestamp}.png"
+        cv2.imwrite(filename, window.photostripImage)
+        window.reset()
 
     def reset(window):
         """
@@ -301,9 +372,17 @@ class Window(QWidget):
         Function: Clears all captured photos, resets counters and timers, releases the webcam if it's still active, and shows the welcome message and start button again to allow for a new photobooth session to begin.
 
         """
+        window.nextButton.setHidden(True)
+        window.white.setHidden(True)
+        window.black.setHidden(True)
+        window.pink.setHidden(True)
+        window.yellow.setHidden(True)
+        window.mint.setHidden(True)
+        window.lavender.setHidden(True)
+       
         #resets everything after showing photostrip
         window.welcome.setHidden(False)
-        window.button.setHidden(False)
+        window.startButton.setHidden(False)
 
         if window.video is not None:
             window.video.release()
@@ -313,7 +392,7 @@ class Window(QWidget):
         window.imageLabel.clear()
 
         window.captureIndex = 0
-        window.countdown = 7
+        window.countdown = 3
        
         window.photos.clear()
 
